85c7d73d4668d1604b393d11c9d9ae9ecfdbfceb
IvanSkripov <ivan.skripov@gmail.com>
2025-06-01 21:03:35 +0300

    M1-L1-First Init commit. Code + Test

---
+++ settings.gradle.kts
@@ -0,0 +1,13 @@
+pluginManagement {
+    val kotlinVersion: String by settings
+    plugins {
+        kotlin("jvm") version kotlinVersion
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+rootProject.name = "otus-first"
+include("M1-L1-First")
+include("M1-L1-First")
