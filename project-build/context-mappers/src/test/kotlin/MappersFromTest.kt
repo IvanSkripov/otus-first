4698eed9e3b695df35ea88fbf767c091058aeb82
IvanSkripov <ivan.skripov@gmail.com>
2025-09-11 21:49:24 +0800

    Update Stubs to UTests

--- project-build/context-mappers/src/test/kotlin/MappersFromTest.kt
+++ project-build/context-mappers/src/test/kotlin/MappersFromTest.kt
@@ -1,58 +1,35 @@
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
-import ru.otus.kotlin.course.api.v1.models.DebugItem
-import ru.otus.kotlin.course.api.v1.models.ImageItem
-import ru.otus.kotlin.course.api.v1.models.ImageUpdateRequest
-import ru.otus.kotlin.course.api.v1.models.Label
+import ru.otus.kotlin.course.api.v1.models.*
 import ru.otus.kotlin.course.common.PsBeContext
 import ru.otus.kotlin.course.common.models.*
-import ru.otus.kotlin.course.common.stubs.PsStubs
+import ru.otus.kotlin.course.common.stubs.*
+import ru.otus.kotlin.course.mappers.exception.WrongStateException
 import ru.otus.kotlin.course.mappers.fromTransport
+import ru.otus.kotlin.course.mappers.toTransport
 import kotlin.test.assertEquals
 
 
 class MappersFromTest {
 
-    private val IMAGE_ID = "123"
-    private val IMAGE_TITLE = "Update Title"
-    private val IMAGE_DESC = "Update Image Description"
-    private val TAGS = mutableListOf("good", "nice")
+    private val items: List<Pair<IRequest, PsBeContext>> = listOf(
+        stubUpdateFromTransport(),
+        stubReadFromTransport(),
+        stubDeleteFromTransport(),
+        stubLinkFromTransport(),
+        stubDownloadFromTransport(),
+        stubSearchFromTransport(),
+    )
 
-    // TODO: Проверить все типы
 
     @Test
-    fun fromTransportUpdate() {
-        val requestUpdate = ImageUpdateRequest(
-            debug = DebugItem(
-                mode = DebugItem.Mode.STUB,
-                stub = DebugItem.Stub.WRONG_LINK),
-            image = ImageItem(
-                imageId = IMAGE_ID,
-                title = IMAGE_TITLE,
-                desc = IMAGE_DESC,
-                tags = TAGS,
-                labels = listOf(Label("author", value = "WhoIsAuthor"))
-            )
-        )
+    fun fromTransport() {
+        items.forEach {
+            println("${it.first}\n\t\t ${it.second}")
+            val obj = PsBeContext()
+            obj.fromTransport(it.first)
+            assertEquals(obj, it.second)
 
-        val expected = PsBeContext (
-            command = PsCommand.UPDATE,
-            workMode = PsWorkMode.STUB,
-            stubCase = PsStubs.WRONG_LINK,
-            request = PsImage(
-                id = PsImageId(IMAGE_ID),
-                title = IMAGE_TITLE,
-                desc = IMAGE_DESC,
-                tags = TAGS,
-                labels = mutableListOf(PsLabel("author", value = "WhoIsAuthor"))
-            )
-        )
-
-        val obj = PsBeContext()
-        obj.fromTransport(requestUpdate)
-
-        assertEquals(obj, expected)
+        }
     }
-
-
 }
--- project-build/context-stubs/src/main/kotlin/PsImageStubs.kt
+++ project-build/context-stubs/src/main/kotlin/PsImageStubs.kt
@@ -8,11 +8,12 @@ import java.net.URI
 private val IMAGE_ID = "123"
 private val IMAGE_TITLE = "Update Title"
 private val IMAGE_DESC = "Update Image Description"
+private val SEARCH_STRING = "Search String is here"
 private val TAGS = mutableListOf("good", "nice")
 private val ERORS = listOf(ResponseErrorValue("1", "4", "2", "3"))
 private val ERORS_PS = mutableListOf(PsError("1", "2", "3", "4"))
 private val LABELS = listOf(Label("1", "2", "3"))
-private val LABESL_PS = mutableListOf(PsLabel("1", "2", "3"))
+private val LABELS_PS = mutableListOf(PsLabel("1", "2", "3"))
 
 
 
@@ -38,6 +39,8 @@ fun stubResponseError(stub: PsStubs ): PsBeContext {
     }
 }
 
+
+// ==== To Transport STUB ===============
 fun stubCreateToTransport(): Pair<PsBeContext, ImageCreateResponse> {
     return Pair (
         PsBeContext (
@@ -76,11 +79,11 @@ fun stubReadToTransport(): Pair<PsBeContext, ImageReadResponse> {
     return Pair ( PsBeContext (
         command = PsCommand.READ,
         state = PsState.FINISHING,
-        response = PsImageStubsItems.FULL_PSIMAGE
+        response = PsImageStubsItems.FULL_TO_PSIMAGE
     ),
         ImageReadResponse (
             result = ResponseResult.SUCCESS,
-            image = PsImageStubsItems.FULL_IMAGE
+            image = PsImageStubsItems.FULL_TO_IMAGE
         )
     )
 }
@@ -193,11 +196,11 @@ fun stubSearchToTransport(): Pair<PsBeContext, ImageSearchResponse> {
         PsBeContext(
             command = PsCommand.SEARCH,
             state = PsState.FINISHING,
-            responseList = mutableListOf(PsImageStubsItems.FULL_PSIMAGE, PsImageStubsItems.FULL_PSIMAGE)
+            responseList = mutableListOf(PsImageStubsItems.FULL_TO_PSIMAGE, PsImageStubsItems.FULL_TO_PSIMAGE)
         ),
         ImageSearchResponse(
             result = ResponseResult.SUCCESS,
-            list = listOf(PsImageStubsItems.FULL_IMAGE, PsImageStubsItems.FULL_IMAGE)
+            list = listOf(PsImageStubsItems.FULL_TO_IMAGE, PsImageStubsItems.FULL_TO_IMAGE)
         )
     )
 }
@@ -208,11 +211,11 @@ fun stubSearchFailedToTransport(): Pair<PsBeContext, ImageSearchResponse> {
             command = PsCommand.SEARCH,
             state = PsState.FAILING,
             errors = ERORS_PS,
-            responseList = mutableListOf(PsImageStubsItems.FULL_PSIMAGE, PsImageStubsItems.FULL_PSIMAGE)
+            responseList = mutableListOf(PsImageStubsItems.FULL_TO_PSIMAGE, PsImageStubsItems.FULL_TO_PSIMAGE)
         ),
         ImageSearchResponse(
             result = ResponseResult.ERROR,
-            list = listOf(PsImageStubsItems.FULL_IMAGE, PsImageStubsItems.FULL_IMAGE),
+            list = listOf(PsImageStubsItems.FULL_TO_IMAGE, PsImageStubsItems.FULL_TO_IMAGE),
             errors = ERORS
         )
     )
@@ -262,7 +265,7 @@ fun stubLabelsToTransport(): Pair<PsBeContext, LabelsResponse> {
             response = PsImage(
                 id = PsImageId(IMAGE_ID), // No need. Simple Test
                 tags = TAGS,              // No need. Simple Test
-                labels = LABESL_PS
+                labels = LABELS_PS
             )
         ),
         LabelsResponse(
@@ -281,7 +284,7 @@ fun stubLabelsFailedToTransport(): Pair<PsBeContext, LabelsResponse> {
             response = PsImage(
                 id = PsImageId(IMAGE_ID), // No need. Simple Test
                 tags = TAGS,              // No need. Simple Test
-                labels = LABESL_PS
+                labels = LABELS_PS
             )
         ),
         LabelsResponse(
@@ -307,23 +310,121 @@ fun stubDownloadFailedToTransport(): Pair<PsBeContext, ImageDownloadErrorRespons
     )
 }
 
+// ==== From Transport STUB ===============
+
+fun stubUpdateFromTransport(): Pair<ImageUpdateRequest, PsBeContext> {
+    return Pair(
+        ImageUpdateRequest(
+            debug = PsImageStubsItems.DBG_WRONG,
+            image = PsImageStubsItems.FULL_FROM_IMAGE
+        ),
+
+        PsBeContext(
+            command = PsCommand.UPDATE,
+            workMode = PsWorkMode.STUB,
+            stubCase = PsStubs.WRONG_LINK,
+            request = PsImageStubsItems.FULL_FROM_PSIMAGE
+        )
+    )
+}
+
+fun stubReadFromTransport(): Pair<ImageReadRequest, PsBeContext> {
+    return Pair(
+        ImageReadRequest(
+            debug = PsImageStubsItems.DBG_WRONG,
+            imageId = IMAGE_ID
+        ),
+        PsBeContext(
+            command = PsCommand.READ,
+            workMode = PsWorkMode.STUB,
+            stubCase = PsStubs.WRONG_LINK,
+            request = PsImageStubsItems.SIMPLE_REQUEST
+        )
+    )
+}
+
+fun stubDeleteFromTransport(): Pair<ImageDeleteRequest, PsBeContext> {
+    return Pair(
+        ImageDeleteRequest(
+            debug = PsImageStubsItems.DBG_WRONG,
+            imageId = IMAGE_ID
+        ),
+        PsBeContext(
+            command = PsCommand.DELETE,
+            workMode = PsWorkMode.STUB,
+            stubCase = PsStubs.WRONG_LINK,
+            request = PsImageStubsItems.SIMPLE_REQUEST
+        )
+    )
+}
+
+fun stubLinkFromTransport(): Pair<ImageLinkRequest, PsBeContext> {
+    return Pair(
+        ImageLinkRequest(
+            debug = PsImageStubsItems.DBG_WRONG,
+            imageId = IMAGE_ID
+        ),
+        PsBeContext(
+            command = PsCommand.LINK,
+            workMode = PsWorkMode.STUB,
+            stubCase = PsStubs.WRONG_LINK,
+            request = PsImageStubsItems.SIMPLE_REQUEST
+        )
+    )
+}
+
+fun stubDownloadFromTransport(): Pair<ImageDownloadRequest, PsBeContext> {
+    return Pair(
+        ImageDownloadRequest(
+            debug = PsImageStubsItems.DBG_WRONG,
+            imageId = IMAGE_ID
+        ),
+        PsBeContext(
+            command = PsCommand.DOWNLOAD,
+            workMode = PsWorkMode.STUB,
+            stubCase = PsStubs.WRONG_LINK,
+            request = PsImageStubsItems.SIMPLE_REQUEST
+        )
+    )
+}
+
+fun stubSearchFromTransport(): Pair<ImageSearchRequest, PsBeContext> {
+    return Pair(
+        ImageSearchRequest(
+            debug = PsImageStubsItems.DBG_OK,
+            search = ImageSearchObject(SEARCH_STRING)
+        ),
+        PsBeContext(
+            command = PsCommand.SEARCH,
+            workMode = PsWorkMode.TEST,
+            stubCase = PsStubs.SUCCESS,
+            filterString = SEARCH_STRING
+        )
+    )
+}
+
+// ==== STUB Helpers ===============
+
 object PsImageStubsItems {
-    val DBG = DebugItem (
+    val DBG_WRONG = DebugItem (
         mode = DebugItem.Mode.STUB,
         stub = DebugItem.Stub.WRONG_LINK)
 
-    val FULL_PSIMAGE = PsImage(
+    val DBG_OK = DebugItem (
+        mode = DebugItem.Mode.TEST,
+        stub = DebugItem.Stub.SUCCESS)
+
+    val FULL_TO_PSIMAGE = PsImage(
         id = PsImageId(IMAGE_ID),
         title = IMAGE_TITLE,
         desc =  IMAGE_DESC,
         tags = TAGS,
-        labels = LABESL_PS,
+        labels = LABELS_PS,
         imageUrl = "www.google.com",
         previewUrl = "www.yandex.ru"
-
     )
 
-    val FULL_IMAGE = Image (
+    val FULL_TO_IMAGE = Image (
         imageId = IMAGE_ID,
         preview = URI("www.yandex.ru"),
         image = URI("www.google.com"),
@@ -332,4 +433,23 @@ object PsImageStubsItems {
         tags = TAGS,
         labels = LABELS
     )
+
+    val FULL_FROM_IMAGE = ImageItem(
+        imageId = IMAGE_ID,
+        title = IMAGE_TITLE,
+        desc = IMAGE_DESC,
+        tags = TAGS,
+        labels = LABELS
+    )
+
+    val FULL_FROM_PSIMAGE = PsImage(
+        id = PsImageId(IMAGE_ID),
+        title = IMAGE_TITLE,
+        desc = IMAGE_DESC,
+        tags = TAGS,
+        labels = LABELS_PS
+    )
+
+    val SIMPLE_REQUEST = PsImage(id = PsImageId(IMAGE_ID))
+
 }
