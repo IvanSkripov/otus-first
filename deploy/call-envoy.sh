b5ba9027fbb05557219728c0c962d094a872531f
IvanSkripov <ivan.skripov@gmail.com>
2025-12-12 23:13:54 +0300

    Prepare deploy (Part 4)

--- deploy/call-envoy.sh
+++ deploy/call-envoy.sh
@@ -8,7 +8,9 @@ TOKEN=$(./keycloak-tokens.sh)
 #  -H "x-client-request-id: 1235" \
 #  http://localhost:8080/
 
-curl -H "Authorization: Bearer ${TOKEN}" \
+echo "TOKEN=$TOKEN"
+
+curl -vvv -H "Authorization: Bearer ${TOKEN}" \
   -H "X-Request-ID: 1234" \
   -H "x-client-request-id: 1235" \
   http://localhost:8080/actuator
