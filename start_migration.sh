d1435f3b812cdc29e1a903e0661d1aa9e1d2300f
IvanSkripov <ivan.skripov@gmail.com>
2025-12-13 15:04:53 +0300

    Prepare deploy (Part 9)

---
+++ start_migration.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+
+export VERSION=0.0.1
+docker network create -d bridge migration_network
+docker build -t migration:$VERSION -f ./dockers/liqbase.Dockerfile ./../
+set -a; source .env; set +a
+docker run --name some-postgres --network=migration_network -e POSTGRES_USER=$DB_USER -e POSTGRES_PASSWORD=$DB_PASS -d -p 5432:5432 postgres:15.4
+docker run --network=migration_network -e LIQUIBASE_COMMAND_URL=MIGRATION_URL -e LIQUIBASE_COMMAND_USERNAME=$DB_USER -e LIQUIBASE_COMMAND_PASSWORD=$DB_PASS migration:$VERSION
